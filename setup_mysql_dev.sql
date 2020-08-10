2869757ee9543f7fdc028d1d2c02f47f3f9d44a6
UsmanGTA <usmangta@gmail.com>
2020-08-10 16:03:35 +0000

    added localhost in front of user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,6 +7,6 @@
 -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db' TO 'hbnb_dev';
-GRANT SELECT PRIVILEGES ON 'performance_schema' TO 'hbnb_dev';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db' TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGES ON 'performance_schema' TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
