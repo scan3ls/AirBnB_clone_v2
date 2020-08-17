a8f26efe3bdb03846fb5c96011ce881484d54a37
UsmanGTA <usmangta@gmail.com>
2020-08-17 07:13:35 +0000

    added initial commit for task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""https://www.usmanjabbar.com/"""
+
+
+def do_pack():
+    """
+    ---------------
+    METHOD: DO_PACK
+    ---------------
+    DESCRIPTION:
+        Creates a tar compressed file
+        locally containing the content
+        from /web_static/ and returns the
+        path of where the compressed file
+        has been stored.
+    NOTES:
+        - Output tar files are stored inside
+        the versions/ directory.
+        - Output file would be called
+        "web_static_" + "current time" + ".tgz"
+        - If the compression fails, None's returned.
+    """
+    from fabric.api import local
+    from datetime.datetime import now as time
+    from os.path import isdir
+
+    file = "web_static_" + time().strftime("%Y%m%d%H%M%S") + ".tgz"
+
+    if isdir('/versions'):
+        local('mkdir versions')
+
+    try:
+        local('tar -cfv {} versions/{}'.format(file, file))
+        path = "versions/" + file
+        return path
+    except:
+        return None
