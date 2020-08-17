4b8e5c5a19ea6a12ef5fab0e5479d1a452d1b10b
UsmanGTA <usmangta@gmail.com>
2020-08-17 07:29:44 +0000

    fixed shebang

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,5 +10,5 @@ mkdir -p /data/web_static/releases/test
 echo "I'm Alive!" > /data/web_static/shared/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed '/^\tserver_name*/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/^\tserver_name*/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 service nginx restart 
