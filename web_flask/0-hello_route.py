1691d89ec58e1393992049d22e9bc90d4e314e6b
scan3ls <scan3ls@gmail.com>
2020-08-31 22:05:50 +0000

    pep8 fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,8 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
-app.run('0.0.0.0','5000', False, None)
+
+app.run('0.0.0.0', '5000', False, None)
