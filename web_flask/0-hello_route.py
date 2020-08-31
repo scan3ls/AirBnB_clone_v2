44b94ec2eb48c80e4541d4640df941299ba330c7
scan3ls <scan3ls@gmail.com>
2020-08-31 21:57:15 +0000

    Add task

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Flask app """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+app.run('0.0.0.0','5000', False, None)
